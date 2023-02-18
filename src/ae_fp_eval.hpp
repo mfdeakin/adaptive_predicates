d690e0062518c7e74d49d994669fcb275069eea1
Michael Deakin <mfdeakin@cs.unc.edu>
2023-02-17 17:16:17 -0800

    A C++23 implementation of Shewchuk's expand-sum, called merge sum here

--- src/ae_fp_eval.hpp
+++ src/ae_fp_eval.hpp
@@ -34,7 +34,8 @@ template <std::floating_point eval_type, typename E>
   requires expr_type<E> || arith_number<E>
 constexpr eval_type exactfp_eval(E &&e) noexcept {
   if constexpr (is_expr<std::remove_reference_t<E>>::value) {
-    std::size_t storage_needed = num_partials_for_exact(std::forward<E>(e));
+    const std::size_t storage_needed =
+        num_partials_for_exact(std::forward<E>(e));
     std::vector<eval_type> partial_results(storage_needed, eval_type(0));
     exactfp_eval_impl<eval_type>(
         std::forward<E>(e),
@@ -52,6 +53,10 @@ void sparse_mult(std::span<eval_type> storage_left,
                  std::span<eval_type> storage_right,
                  std::span<eval_type> storage_mult);
 
+template <std::floating_point eval_type>
+void merge_sum(std::span<eval_type> storage, std::span<eval_type> storage_left,
+               std::span<eval_type> storage_right);
+
 template <std::floating_point eval_type, typename E_>
   requires expr_type<E_> || arith_number<E_>
 constexpr void
@@ -66,13 +71,14 @@ exactfp_eval_impl(E_ &&e, std::span<eval_type> partial_results) noexcept {
         partial_results.subspan(reserve_left, reserve_right);
     exactfp_eval_impl<eval_type>(e.rhs(), storage_right);
     using Op = typename E::Op;
-    // We only need compute anything more for subtraction and multiplication,
-    // since we're assuming division doesn't exist, and addition is already
-    // represented by the vector of fp values
-    if constexpr (std::is_same_v<std::minus<>, Op>) {
-      for (eval_type &v : storage_right) {
-        v = -v;
+    if constexpr (std::is_same_v<std::plus<>, Op> ||
+                  std::is_same_v<std::minus<>, Op>) {
+      if constexpr (std::is_same_v<std::minus<>, Op>) {
+        for (eval_type &v : storage_right) {
+          v = -v;
+        }
       }
+      merge_sum(partial_results, storage_left, storage_right);
     } else if constexpr (std::is_same_v<std::multiplies<>, Op>) {
       const auto storage_mult = partial_results.last(
           partial_results.size() - reserve_left - reserve_right);
@@ -80,6 +86,38 @@ exactfp_eval_impl(E_ &&e, std::span<eval_type> partial_results) noexcept {
     }
   } else if constexpr (!std::is_same_v<additive_id, E>) {
     *partial_results.begin() = eval_type(e);
+  }
+}
+
+template <std::floating_point eval_type>
+constexpr std::pair<eval_type, eval_type>
+dekker_sum_unchecked(const eval_type &lhs, const eval_type &rhs);
+
+template <std::floating_point eval_type>
+constexpr std::pair<eval_type, eval_type> knuth_sum(const eval_type &lhs,
+                                                    const eval_type &rhs);
+
+template <std::floating_point eval_type>
+void merge_sum(std::span<eval_type> storage, std::span<eval_type> storage_left,
+               std::span<eval_type> storage_right) {
+  if (storage.size() > 1) {
+    // std::ranges::inplace_merge(
+    //         storage_left.begin(), storage_right.begin(),
+    //         storage_right.end(),
+    //         [](eval_type l, eval_type r) { return std::abs(l) <
+    //         std::abs(r); });
+    std::ranges::sort(storage, [](eval_type l, eval_type r) {
+      return std::abs(l) < std::abs(r);
+    });
+    auto [Q, q] = dekker_sum_unchecked(storage[0], storage[1]);
+    for (auto [g, t] :
+         std::ranges::views::zip(storage | std::views::drop(2), storage)) {
+      const auto [R, h] = dekker_sum_unchecked(g, q);
+      t = h;
+      std::tie(Q, q) = knuth_sum(Q, R);
+    }
+    *(storage.end() - 2) = q;
+    *(storage.end() - 1) = Q;
   }
 }
 
@@ -136,6 +174,25 @@ void overflow_write(const eval_type &v, I &itr1, const I &itr1_end, I &itr2) {
     *itr2 = v;
     ++itr2;
   }
+}
+
+template <std::floating_point eval_type>
+constexpr std::pair<eval_type, eval_type>
+dekker_sum_unchecked(const eval_type &lhs, const eval_type &rhs) {
+  const eval_type upper = lhs + rhs;
+  const eval_type rounding_err = upper - lhs;
+  return {upper, rhs - rounding_err};
+}
+
+template <std::floating_point eval_type>
+constexpr std::pair<eval_type, eval_type> knuth_sum(const eval_type &lhs,
+                                                    const eval_type &rhs) {
+  const eval_type upper = lhs + rhs;
+  const eval_type rounding_err_rhs = upper - lhs;
+  const eval_type rounding_err_lhs = upper - rounding_err_rhs;
+  const eval_type lhs_roundoff = rhs - rounding_err_rhs;
+  const eval_type rhs_roundoff = lhs - rounding_err_lhs;
+  return {upper, lhs_roundoff + rhs_roundoff};
 }
 
 template <std::floating_point eval_type>
