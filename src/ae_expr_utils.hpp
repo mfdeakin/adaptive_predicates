e6ba966dab043765df513c7265b64086b2add3f4
Michael Deakin <mfdeakin@cs.unc.edu>
2023-03-02 18:37:00 -0800

    Add fmt output support for expressions
    Also don't use gcc-13 since it doesn't currently work with fmt
    formatter specializations

--- src/ae_expr_utils.hpp
+++ src/ae_expr_utils.hpp
@@ -63,4 +63,84 @@ template <typename E_> consteval std::size_t num_partials_for_exact() {
 
 } // namespace adaptive_expr
 
+template <typename Op, typename LHS, typename RHS>
+struct fmt::formatter<adaptive_expr::arith_expr<Op, LHS, RHS>> {
+  template <typename ParseContext> constexpr auto parse(ParseContext &ctx) {
+    return ctx.begin();
+  }
+
+  template <typename FormatContext>
+  auto format(const adaptive_expr::arith_expr<Op, LHS, RHS> &e, FormatContext &ctx) const {
+    if constexpr (std::floating_point<LHS> && std::floating_point<RHS>) {
+      return format_to(ctx.out(), "({: .17f} {} {: .17f})", e.lhs(), Op(), e.rhs());
+    } else if constexpr (std::floating_point<LHS>) {
+      return format_to(ctx.out(), "({: .17f} {} {})", e.lhs(), Op(), e.rhs());
+    } else if constexpr (std::floating_point<RHS>) {
+      return format_to(ctx.out(), "({} {} {: .17f})", e.lhs(), Op(), e.rhs());
+    } else {
+      return format_to(ctx.out(), "({} {} {})", e.lhs(), Op(), e.rhs());
+    }
+  }
+};
+
+template <>
+struct fmt::formatter<adaptive_expr::additive_id> {
+  template <typename ParseContext> constexpr auto parse(ParseContext &ctx) {
+    return ctx.begin();
+  }
+
+  template <typename FormatContext>
+  auto format(const adaptive_expr::additive_id &, FormatContext &ctx) const {
+    return fmt::format_to(ctx.out(), "0");
+  }
+};
+
+template <>
+struct fmt::formatter<std::plus<>> {
+  template <typename ParseContext> constexpr auto parse(ParseContext &ctx) {
+    return ctx.begin();
+  }
+
+  template <typename FormatContext>
+  auto format(const std::plus<> &, FormatContext &ctx) const {
+    return fmt::format_to(ctx.out(), "+");
+  }
+};
+
+template <>
+struct fmt::formatter<std::minus<>> {
+  template <typename ParseContext> constexpr auto parse(ParseContext &ctx) {
+    return ctx.begin();
+  }
+
+  template <typename FormatContext>
+  auto format(const std::minus<> &, FormatContext &ctx) const {
+    return fmt::format_to(ctx.out(), "-");
+  }
+};
+
+template <>
+struct fmt::formatter<std::multiplies<>> {
+  template <typename ParseContext> constexpr auto parse(ParseContext &ctx) {
+    return ctx.begin();
+  }
+
+  template <typename FormatContext>
+  auto format(const std::multiplies<> &, FormatContext &ctx) const {
+    return fmt::format_to(ctx.out(), "*");
+  }
+};
+
+template <>
+struct fmt::formatter<std::divides<>> {
+  template <typename ParseContext> constexpr auto parse(ParseContext &ctx) {
+    return ctx.begin();
+  }
+
+  template <typename FormatContext>
+  auto format(const std::divides<> &, FormatContext &ctx) const {
+    return fmt::format_to(ctx.out(), "/");
+  }
+};
+
 #endif // ADAPTIVE_PREDICATES_AE_EXPR_UTILS_HPP
