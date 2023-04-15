f403ce7ba531e3208d02f9308da2339bc942d2e1
Michael Deakin <mfdeakin@cs.unc.edu>
2023-04-15 13:39:11 -0700

    Add the kokkos reference implementation of mdspan
    Also use mdspan rather than an array of arrays

--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -4,6 +4,16 @@
 #include <tuple>
 #include <type_traits>
 
+#if __cpp_lib_mdspan >= 202207L
+#include <mdspan>
+using std::extents;
+using std::mdspan;
+#else
+#include <mdspan.hpp>
+using std::experimental::extents;
+using std::experimental::mdspan;
+#endif
+
 #include <fmt/format.h>
 
 #include <catch2/catch_test_macros.hpp>
@@ -25,9 +35,10 @@ using namespace adaptive_expr;
 // the determinant is then the determinant of the resulting matrix divided by
 // the determinants of the scaling matrices
 template <typename eval_type, typename row_exprs, std::size_t N>
-constexpr auto determinant(std::array<std::array<row_exprs, N>, N> mtx,
-                           const int swaps = 0) {
-  const auto v = balance_expr(mtx[0][0]);
+constexpr auto
+determinant(const mdspan<row_exprs, extents<std::size_t, N, N>> mtx,
+            const int swaps = 0) {
+  const auto v = balance_expr(mtx[0, 0]);
   const auto approx_val = adaptive_eval<eval_type>(v);
   if constexpr (N == 1) {
     return approx_val;
@@ -36,31 +47,35 @@ constexpr auto determinant(std::array<std::array<row_exprs, N>, N> mtx,
       if (swaps == N - 1) {
         return eval_type{0};
       } else {
-        std::swap(mtx[0], mtx[swaps + 1]);
+        for (std::size_t col = 0; col < N; ++col) {
+          std::swap(mtx[0, col], mtx[swaps + 1, col]);
+        }
         return determinant<eval_type>(mtx, swaps + 1);
       }
     } else {
-      using submtx_expr = decltype(mult_expr(v, std::declval<row_exprs>()) -
-                                   mult_expr(std::declval<row_exprs>(),
-                                             std::declval<row_exprs>()));
-      std::array<std::array<submtx_expr, N - 1>, N - 1> submtx;
+      using submtx_expr =
+          decltype(mult_expr(v, mtx[0, 0]) - mult_expr(v, mtx[0, 0]));
+      std::array<submtx_expr, (N - 1) * (N - 1)> submtx;
       for (std::size_t i = 1; i < N; ++i) {
-        const auto s = mtx[i][0];
+        const auto s = balance_expr(mtx[i, 0]);
         for (std::size_t j = 1; j < N; ++j) {
-          submtx[i - 1][j - 1] =
-              mult_expr(v, mtx[i][j]) - mult_expr(s, mtx[0][j]);
+          submtx[(i - 1) * (N - 1) + j - 1] =
+              mult_expr(v, mtx[i, j]) - mult_expr(s, mtx[0, j]);
         }
       }
-      const auto subdet = determinant<eval_type>(submtx);
-      return subdet * std::pow(approx_val, -N + 2) * std::pow(-1.0, swaps);
+      mdspan<submtx_expr, extents<std::size_t, N - 1, N - 1>> submtx_span{
+          submtx.data()};
+      const auto subdet = determinant<eval_type>(submtx_span);
+      return subdet * std::pow(approx_val, -N + 2) * std::pow(-1, swaps);
     }
   }
 }
 
 TEST_CASE("determinant_sign", "[matrix]") {
-  REQUIRE(determinant<double>(
-              std::array{std::array{5.0, 2.0}, std::array{-1.0, 0.0}}) == 2.0);
-  REQUIRE(determinant<double>(std::array{std::array{0.0, 5.0, 2.0},
-                                         std::array{0.0, 1.0, 0.0},
-                                         std::array{1.0, 86.0, 35.0}}) == -2.0);
+  REQUIRE(determinant<double>(mdspan<double, extents<std::size_t, 2, 2>>{
+              std::array{5.0, 2.0, -1.0, 0.0}.data()}) == 2.0);
+  REQUIRE(
+      determinant<double>(mdspan<double, extents<std::size_t, 3, 3>>{
+          std::array{0.0, 5.0, 2.0, 0.0, 1.0, 0.0, 1.0, 86.0, 35.0}.data()}) ==
+      -2.0);
 }
