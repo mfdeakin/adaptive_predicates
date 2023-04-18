690fea53035f1ba832cb134a37a1c672c5dc4278
Michael Deakin <mfdeakin@cs.unc.edu>
2023-04-17 20:16:24 -0700

    Some reorganization, mainly move catch2 benchmarking to it's own executable

--- tests/test_adaptive_expr.cpp
+++ tests/test_adaptive_expr.cpp
@@ -1,21 +1,13 @@
 
-#include <catch2/benchmark/catch_benchmark.hpp>
+#include <random>
+
 #include <catch2/catch_test_macros.hpp>
-#include <catch2/generators/catch_generators.hpp>
-#include <catch2/generators/catch_generators_adapters.hpp>
-#include <catch2/generators/catch_generators_random.hpp>
-#include <catch2/generators/catch_generators_range.hpp>
 
 #include <fmt/format.h>
 
 #include "ae_adaptive_predicate_eval.hpp"
 #include "ae_expr.hpp"
 #include "ae_fp_eval.hpp"
-
-#include "shewchuk.h"
-
-#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
-#include <CGAL/Point_2.h>
 
 #include "double_testing_data.hpp"
 #include "testing_utils.hpp"
@@ -207,9 +199,8 @@ static_assert(std::is_same_v<
               decltype(trim_expr(mult_expr(3.0, 5.1) - mult_expr(0.0, 1.0))),
               decltype(mult_expr(3.0, 5.1) - mult_expr(0.0, 1.0))>);
 static_assert(std::is_same_v<
-              decltype(balance_expr(mult_expr(3.0, 5.1) -
-              mult_expr(0.0, 1.0))), decltype(mult_expr(3.0, 5.1) -
-              mult_expr(0.0, 1.0))>);
+              decltype(balance_expr(mult_expr(3.0, 5.1) - mult_expr(0.0, 1.0))),
+              decltype(mult_expr(3.0, 5.1) - mult_expr(0.0, 1.0))>);
 
 TEST_CASE("expr_template_trim", "[expr_template_rewrite]") {
   REQUIRE(trim_expr(additive_id{} + 5) == 5);
@@ -328,126 +319,4 @@ TEST_CASE("expr_template_eval_simple", "[expr_template_eval]") {
   REQUIRE(result < 0.0);
 
   REQUIRE(!correct_eval<real>(build_orient2d_case(points)));
-}
-
-TEST_CASE("BenchmarkDeterminant", "[benchmark]") {
-  exactinit();
-  const auto points1 = orient2d_cases[0].first;
-  constexpr std::size_t x = 0;
-  constexpr std::size_t y = 1;
-  CGAL::Point_2<CGAL::Exact_predicates_exact_constructions_kernel> pt0(
-      points1[0][x], points1[0][y]);
-  CGAL::Point_2<CGAL::Exact_predicates_exact_constructions_kernel> pt1(
-      points1[1][x], points1[1][y]);
-  CGAL::Point_2<CGAL::Exact_predicates_exact_constructions_kernel> pt2(
-      points1[2][x], points1[2][y]);
-  BENCHMARK("build expr 1") { return build_orient2d_case(points1); };
-  BENCHMARK("no expr floating point 1") {
-    return points1[1][x] * points1[2][y] - points1[1][y] * points1[2][x] -
-           points1[0][x] * points1[2][y] + points1[0][y] * points1[2][x] +
-           points1[0][x] * points1[1][y] - points1[0][y] * points1[1][x];
-  };
-  BENCHMARK("floating point 1") {
-    const auto e = build_orient2d_case(points1);
-    return fp_eval<real>(e);
-  };
-  BENCHMARK("correct or nothing 1") {
-    return correct_eval<real>(build_orient2d_case(points1));
-  };
-  BENCHMARK("exact rounded 1") {
-    const auto e = build_orient2d_case(points1);
-    return exactfp_eval<real>(e);
-  };
-  BENCHMARK("adaptive 1") {
-    const auto e = build_orient2d_case(points1);
-    return adaptive_eval<real>(e);
-  };
-  BENCHMARK("shewchuk floating point 1") {
-    return orient2dfast(points1[0].data(), points1[1].data(),
-                        points1[2].data());
-  };
-  BENCHMARK("shewchuk exact rounded 1") {
-    return orient2d(points1[0].data(), points1[1].data(), points1[2].data());
-  };
-  BENCHMARK("cgal exact rounded 1") {
-    return CGAL::orientation(pt0, pt1, pt2);
-  };
-
-  const auto points2 = orient2d_cases[1].first;
-  pt0 = CGAL::Point_2<CGAL::Exact_predicates_exact_constructions_kernel>(
-      points2[0][x], points2[0][y]);
-  pt1 = CGAL::Point_2<CGAL::Exact_predicates_exact_constructions_kernel>(
-      points2[1][x], points2[1][y]);
-  pt2 = CGAL::Point_2<CGAL::Exact_predicates_exact_constructions_kernel>(
-      points2[2][x], points2[2][y]);
-  BENCHMARK("build expr 2") { return build_orient2d_case(points2); };
-  BENCHMARK("no expr floating point 2") {
-    return points2[1][x] * points2[2][y] - points2[1][y] * points2[2][x] -
-           points2[0][x] * points2[2][y] + points2[0][y] * points2[2][x] +
-           points2[0][x] * points2[1][y] - points2[0][y] * points2[1][x];
-  };
-  BENCHMARK("floating point 2") {
-    const auto e = build_orient2d_case(points2);
-    return fp_eval<real>(e);
-  };
-  BENCHMARK("correct or nothing 2") {
-    return correct_eval<real>(build_orient2d_case(points2));
-  };
-  BENCHMARK("exact rounded 2") {
-    const auto e = build_orient2d_case(points2);
-    return exactfp_eval<real>(e);
-  };
-  BENCHMARK("adaptive 2") {
-    const auto e = build_orient2d_case(points2);
-    return adaptive_eval<real>(e);
-  };
-  BENCHMARK("shewchuk floating point 2") {
-    return orient2dfast(points2[0].data(), points2[1].data(),
-                        points2[2].data());
-  };
-  BENCHMARK("shewchuk exact rounded 2") {
-    return orient2d(points2[0].data(), points2[1].data(), points2[2].data());
-  };
-  BENCHMARK("cgal exact rounded 2") {
-    return CGAL::orientation(pt0, pt1, pt2);
-  };
-
-  const auto points3 = orient2d_cases[15].first;
-  pt0 = CGAL::Point_2<CGAL::Exact_predicates_exact_constructions_kernel>(
-      points3[0][x], points3[0][y]);
-  pt1 = CGAL::Point_2<CGAL::Exact_predicates_exact_constructions_kernel>(
-      points3[1][x], points3[1][y]);
-  pt2 = CGAL::Point_2<CGAL::Exact_predicates_exact_constructions_kernel>(
-      points3[2][x], points3[2][y]);
-  BENCHMARK("build expr 3") { return build_orient2d_case(points3); };
-  BENCHMARK("no expr floating point 3") {
-    return points3[1][x] * points3[2][y] - points3[1][y] * points3[2][x] -
-           points3[0][x] * points3[2][y] + points3[0][y] * points3[2][x] +
-           points3[0][x] * points3[1][y] - points3[0][y] * points3[1][x];
-  };
-  BENCHMARK("floating point 3") {
-    const auto e = build_orient2d_case(points3);
-    return fp_eval<real>(e);
-  };
-  BENCHMARK("correct or nothing 3") {
-    return correct_eval<real>(build_orient2d_case(points3));
-  };
-  BENCHMARK("exact rounded 3") {
-    const auto e = build_orient2d_case(points3);
-    return exactfp_eval<real>(e);
-  };
-  BENCHMARK("adaptive 3") {
-    const auto e = build_orient2d_case(points3);
-    return adaptive_eval<real>(e);
-  };
-  BENCHMARK("shewchuk floating point 3") {
-    return orient2dfast(points3[0].data(), points3[1].data(),
-                        points3[2].data());
-  };
-  BENCHMARK("shewchuk exact rounded 3") {
-    return orient2d(points3[0].data(), points3[1].data(), points3[2].data());
-  };
-  BENCHMARK("cgal exact rounded 3") {
-    return CGAL::orientation(pt0, pt1, pt2);
-  };
 }
--- tests/testing_utils.hpp
+++ tests/testing_utils.hpp
@@ -4,7 +4,21 @@
 
 #include <cmath>
 
+#include "ae_adaptive_predicate_eval.hpp"
 #include "ae_expr.hpp"
+#include "ae_expr_utils.hpp"
+
+#if __cpp_lib_mdspan >= 202207L
+#include <mdspan>
+using std::extents;
+using std::mdarray;
+using std::mdspan;
+#else
+#include <mdspan.hpp>
+using std::experimental::extents;
+using std::experimental::mdarray;
+using std::experimental::mdspan;
+#endif
 
 template <adaptive_expr::arith_number eval_type>
 constexpr bool check_sign(eval_type correct, eval_type check) {
@@ -24,10 +38,88 @@ build_orient2d_case(const std::array<std::array<eval_type, 2>, 3> &points) {
   constexpr std::size_t y = 1;
   const auto cross_expr = [](const std::array<eval_type, 2> &lhs,
                              const std::array<eval_type, 2> &rhs) constexpr {
-    return adaptive_expr::mult_expr(lhs[x], rhs[y]) - adaptive_expr::mult_expr(lhs[y], rhs[x]);
+    return adaptive_expr::mult_expr(lhs[x], rhs[y]) -
+           adaptive_expr::mult_expr(lhs[y], rhs[x]);
   };
   return cross_expr(points[1], points[2]) - cross_expr(points[0], points[2]) +
          cross_expr(points[0], points[1]);
+}
+
+template <adaptive_expr::arith_number eval_type>
+constexpr auto
+build_orient2d_matrix(const std::array<std::array<eval_type, 2>, 3> &points) {
+  mdarray<eval_type, extents<std::size_t, 3, 3>> array;
+  array[0, 0] = points[0][0];
+  array[0, 1] = points[0][1];
+  array[0, 2] = eval_type{1};
+  array[1, 0] = points[1][0];
+  array[1, 1] = points[1][1];
+  array[1, 2] = eval_type{1};
+  array[2, 0] = points[2][0];
+  array[2, 1] = points[2][1];
+  array[2, 2] = eval_type{1};
+  return array;
+}
+
+template <typename eval_type, typename row_exprs, std::size_t N>
+auto exchange_pivot(const mdspan<row_exprs, extents<std::size_t, N, N>> mtx) {
+  eval_type approx_val;
+  decltype(adaptive_expr::balance_expr(mtx[0, 0])) v;
+  std::size_t swap_row;
+  for (swap_row = 0; swap_row < N && approx_val == eval_type{0}; ++swap_row) {
+    v = adaptive_expr::balance_expr(mtx[swap_row + 1, 0]);
+    approx_val = adaptive_expr::adaptive_eval<eval_type>(v);
+  }
+  if (approx_val != eval_type{0}) {
+    for (std::size_t col = 0; col < N; ++col) {
+      std::swap(mtx[0, col], mtx[swap_row, col]);
+    }
+  }
+  return std::pair{v, approx_val};
+}
+
+// Based on LU Factorization, except written to avoid divisions in the exact
+// evaluation.
+// Returns an approximate value with sign matching the sign of the determinant.
+// Terminates as early as possible for a non-full rank matrix returning 0
+//
+// LU factorization decomposes a matrix M into a lower triangular matrix L and
+// an upper triangular matrix U. L has 1's on its diagonal, so the determinant
+// of M is the determinant of U, which is the product of its diagonal.
+// This computes U multiplied by some scaling matrices to avoid divisions;
+// the determinant is then the determinant of the resulting matrix divided by
+// the determinants of the scaling matrices
+template <typename eval_type, typename row_exprs, std::size_t N>
+constexpr eval_type
+determinant(const mdspan<row_exprs, extents<std::size_t, N, N>> mtx) {
+  auto v = adaptive_expr::balance_expr(mtx[0, 0]);
+  eval_type approx_val = adaptive_expr::adaptive_eval<eval_type>(v);
+  if constexpr (N == 1) {
+    return approx_val;
+  } else {
+    bool swap = approx_val == eval_type{0};
+    if (swap) {
+      std::tie(v, approx_val) = exchange_pivot<eval_type>(mtx);
+    }
+    using submtx_expr = decltype(adaptive_expr::mult_expr(v, mtx[0, 0]) -
+                                 adaptive_expr::mult_expr(v, mtx[0, 0]));
+    mdarray<submtx_expr, extents<std::size_t, N - 1, N - 1>> submtx;
+    for (std::size_t i = 1; i < N; ++i) {
+      const auto s = adaptive_expr::balance_expr(mtx[i, 0]);
+      for (std::size_t j = 1; j < N; ++j) {
+        submtx[i - 1, j - 1] = adaptive_expr::mult_expr(v, mtx[i, j]) -
+                               adaptive_expr::mult_expr(s, mtx[0, j]);
+      }
+    }
+    const mdspan<submtx_expr, extents<std::size_t, N - 1, N - 1>> submtx_span{
+        submtx.data()};
+    const eval_type subdet = determinant<eval_type>(submtx_span);
+    eval_type det = subdet / std::pow(approx_val, N - 2);
+    if (swap) {
+      det *= eval_type{-1};
+    }
+    return det;
+  }
 }
 
 #endif // TESTING_UTILS_HPP
